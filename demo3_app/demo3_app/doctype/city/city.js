c565801fc781f553ea7f9182f3bc16ed2d484d87
Ami_Trambadiya <amitrambadiya1803@gmail.com>
2022-09-13 17:44:34 +0530

    added a new files

---
+++ demo3_app/demo3_app/doctype/city/city.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Ami and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('City', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ demo3_app/demo3_app/doctype/registration/registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Ami and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Registration', {
+	// refresh: function(frm) {
+
+	// }
+});
